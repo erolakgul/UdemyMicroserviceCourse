e5e51e8005b554a25d47ab7d7319226b997ea266
EROL AKGÜL <erolakgul88@gmail.com>
2023-05-15 11:25:52 +0300

    kategory kurs ve feature class ları tanımlandı

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Model/Course.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace FreeCourse.Services.Catalog.Model
+{
+    public class Course
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+        public string? Picture { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedTime { get; set; }
+
+        public string? UserId { get; set; } // identity server da kullanıcı id si de string olarak tutulacak
+
+        public Feature? Feature { get; set; } // bire bir ilişkiyi tutması için
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? CategoryId { get; set; }  // category class ının id sini fk olarak tutması için
+
+        [BsonIgnore]                            // mongodb ye kaydetme demek, serialize edilmesini engeller
+        public Category? Category { get; set; } // sadece kursları dönerken kategorileri de dönmesi için
+    }
+}
---
+++ Services/Catalog/FreeCourse.Services.Catalog/Model/Feature.cs
@@ -0,0 +1,9 @@
+﻿namespace FreeCourse.Services.Catalog.Model
+{
+    // one to one relation daki yerinden dolayı ayrı bir class
+    public class Feature
+    {
+        public int Duration { get; set; }
+        public bool IsDownload { get; set; } // download edilebilir mi edilemez mi ?
+    }
+}
