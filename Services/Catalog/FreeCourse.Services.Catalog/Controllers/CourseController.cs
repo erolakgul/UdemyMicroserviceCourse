36c72f2d2bc4e8bcbbdc759af31542a36544f352
EROL AKGÜL <erolakgul88@gmail.com>
2023-05-17 13:18:27 +0300

    class library için net core 6 seçimi yapıldı, işlemlere oradan devam edeceğiz.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CourseController.cs
@@ -0,0 +1,21 @@
+﻿using FreeCourse.Services.Catalog.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+
+
+    }
+}
---
+++ Shared/FreeCourse.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeCourse.Shared.ControllerBases
+{
+    public class CustomBaseController 
+    {
+
+    }
+}
