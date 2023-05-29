5dac8ceb7f213982071cbabec74af1a942529a8c
erol.akgul <erolakgul88@gmail.com>
2023-05-29 11:59:59 +0300

    authentication parametresi program cs te verildi,authorize attribute ü yine service tarafında addcontroller da tanımlandı, IdentityServerURL parametresi appsettingse e tanımlandı

--- Services/PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
@@ -1,8 +1,23 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#region  authentication iþlemi için bir þema belirliyoruz
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(option =>
+{
+    option.Authority = builder.Configuration.GetValue<string>("IdentityServerURL");
+    option.Audience = "photostock_catalog"; // identityserver config apiresources, token içerisindeki bu bilgi sayesinde yetkili olup olmadýðýný anlayacaðýz
+    option.RequireHttpsMetadata = false; // https kullanmadðýmýz için kapalý yapýyoruz
+});
+#endregion
 
-builder.Services.AddControllers();
+// Add services to the container.
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter()); // tüm controller lara authorize attribute ünü ekle
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,6 +34,7 @@ if (app.Environment.IsDevelopment())
 // photos klasörüne eriþim ayarý
 app.UseStaticFiles();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
