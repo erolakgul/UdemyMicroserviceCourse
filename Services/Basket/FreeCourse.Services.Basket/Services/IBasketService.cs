57b8a10766c98e3405d7182cd265b8757147e839
erol.akgul <erolakgul88@gmail.com>
2023-05-30 10:22:55 +0300

    IBasketService interface i oluşturuldu

---
+++ Services/Basket/FreeCourse.Services.Basket/Services/IBasketService.cs
@@ -0,0 +1,12 @@
+﻿using FreeCourse.Services.Basket.Dtos;
+using FreeCourse.Shared.Dtos;
+
+namespace FreeCourse.Services.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<Response<BasketHeadDto>> GetBasketByUserIdAsync(string? userId);
+        Task<Response<bool>> SaveOrUpdateAsync(BasketHeadDto basketHeadDto);
+        Task<Response<bool>> DeleteAsync(string? userId);
+    }
+}
