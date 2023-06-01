8c58b1427d82735a94329a44c957864105b57612
erol.akgul <erolakgul88@gmail.com>
2023-06-01 15:38:01 +0300

    IDiscountService methodları tanımlandı

---
+++ Services/Discount/FreeCourse.Services.Discount/Services/Interfaces/IDiscountService.cs
@@ -0,0 +1,15 @@
+﻿using FreeCourse.Services.Discount.Models;
+using FreeCourse.Shared.Dtos;
+
+namespace FreeCourse.Services.Discount.Services.Interfaces
+{
+    public interface IDiscountService
+    {
+        Task<Response<List<Discounts>>> GetAllDiscountsAsync();
+        Task<Response<Discounts>> GetDiscountsByIdAsync(int id);
+        Task<Response<NoContent>> Save(Discounts discounts);
+        Task<Response<NoContent>> Update(Discounts discounts);
+        Task<Response<NoContent>> Delete(int id);
+        Task<Response<Discounts>> GetByCodeAndUserIdAsync(string code, string userId);
+    }
+}
