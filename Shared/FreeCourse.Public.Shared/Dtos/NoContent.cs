c90071ab9dc3e2d4681e231b676d5678eeaa8f01
erol.akgul <erolakgul88@gmail.com>
2023-06-01 13:20:58 +0300

    discount api uygulaması oluşturuldu, portu 5014 olarak ayarlndı,

--- Shared/FreeCourse.Public.Shared/Dtos/NoContent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FreeCourse.Public.Shared.Dtos
-{
-    /// <summary>
-    /// responsedto dan nesne dönmek istenilmiyorsa başarılı olduğunda bu class gönderilecek
-    /// </summary>
-    public class NoContent
-    {
-    }
-}
