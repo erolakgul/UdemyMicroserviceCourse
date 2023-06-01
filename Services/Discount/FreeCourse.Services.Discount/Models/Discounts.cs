e93b45773497a895718940a04edfbfcdf9dfac3e
erol.akgul <erolakgul88@gmail.com>
2023-06-01 13:37:06 +0300

    db nesnesi discount için class oluşturuldu, dapper paketi yüklendi

---
+++ Services/Discount/FreeCourse.Services.Discount/Models/Discounts.cs
@@ -0,0 +1,13 @@
+﻿namespace FreeCourse.Services.Discount.Models
+{
+    [Dapper.Contrib.Extensions.Table("discounts")]
+    public class Discounts
+    {
+        public int Id { get; set; }
+        public string? UserId { get; set; }
+        public int Rate { get; set; }    // discount rate
+        public string? Code { get; set; } // discount code
+        public DateTime ValidDate { get; set; } // last valid date for code
+
+    }
+}
