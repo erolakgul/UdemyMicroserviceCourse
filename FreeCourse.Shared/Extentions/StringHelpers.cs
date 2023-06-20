eff389cc7edc0946b703328767be7b544ca3e93a
erol.akgul <erolakgul88@gmail.com>
2023-06-20 10:48:47 +0300

    string helper oluşturldu, ocelot için development ve production json dosyaları oluşturuldu, service lere ocelot eklendi

---
+++ FreeCourse.Shared/Extentions/StringHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace FreeCourse.Shared.Extentions
+{
+    public static class StringHelpers
+    {
+        /// <summary>
+        /// toupper first letter of string
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string CapitalizeFirstLetter(this String input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            return input.Substring(0, 1).ToUpper(CultureInfo.CurrentCulture) +
+                input.Substring(1, input.Length - 1);
+        }
+    }
+}
--- Gateways/Web/FreeCourse.Gateways.WebApi/Program.cs
+++ Gateways/Web/FreeCourse.Gateways.WebApi/Program.cs
@@ -1,6 +1,24 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+#region ocelot service
+
+IConfiguration configuration = new ConfigurationBuilder()
+                            .AddJsonFile($"Configuration.{builder.Environment.EnvironmentName}.json", true, true)
+                            .Build();
+
+builder.Services.AddOcelot(configuration);
+
+#endregion
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+#region ocelot middleware
+await app.UseOcelot();
+#endregion
 
 app.Run();
